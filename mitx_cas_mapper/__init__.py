cf15dd6c09bf81d09ed3eba5e377acc258b6ff0b
yu <yu@yu-OptiPlex-3010.(none)>
2014-10-28 10:02:19 +0800

    kk

--- mitx_cas_mapper/__init__.py
+++ mitx_cas_mapper/__init__.py
@@ -1,16 +1,31 @@
-"""
-This is used as an attribute mapper
-callable for django-cas per the README at 
-https://github.com/mitocw/django-cas.
-""" 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-VERSION = "0.0.1"
+"""Django CAS 1.0/2.0 authentication backend"""
 
+from django.conf import settings
+
+__all__ = []
+
+_DEFAULTS = {
+    'CAS_ADMIN_PREFIX': None,
+    'CAS_EXTRA_LOGIN_PARAMS': None,
+    'CAS_IGNORE_REFERER': False,
+    'CAS_LOGOUT_COMPLETELY': True,
+    'CAS_REDIRECT_URL': '/',
+    'CAS_RETRY_LOGIN': False,
+    'CAS_PROXY_CALLBACK': None,
+    'CAS_SERVER_URL': None,
+    'CAS_VERSION': '2',
+}
 CAS_URI = 'http://192.168.2.103:8081/cas'
 NSMAP = {'cas': CAS_URI}
 CAS = '{%s}' % CAS_URI
+
+for key,value in _DEFAULTS.iteritems():
+    try:
+        getattr(settings,key)
+    except AttributeError:
+        setattr(settings,key,value)
+    except ImportError:
+        pass
 
 def populate_user(user, authentication_response):
     if authentication_response.find(CAS + 'authenticationSuccess/'  + CAS + 'attributes'  , namespaces=NSMAP) is not None:
@@ -31,3 +46,4 @@ def populate_user(user, authentication_response):
         if attr.find(CAS + 'email', NSMAP) is not None:
             user.email = attr.find(CAS + 'email', NSMAP).text
     pass
+
